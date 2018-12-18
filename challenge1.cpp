677e5a9ab8b889fd3cf688123628ea377b802581
hoborja <hobor.jamie@gmail.com>
2018-12-18 03:39:56 -0500

    initial Commit

---
+++ challenge1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+  using namespace std;
+
+void board(){
+    system("cls");
+
+    cout << endl;
+
+    cout << "     |     |     " <<endl;
+    cout << "     |     |     " <<endl;
+    cout << "=================" <<endl;
+    cout << "     |     |     " <<endl;
+    cout << "     |     |     " <<endl;
+    cout << "=================" <<endl;
+    cout << "     |     |     " <<endl;
+    cout << "     |     |     " <<endl;
+
+
+
+
+}
